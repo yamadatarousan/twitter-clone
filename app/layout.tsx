cba4f2faff91c5e7894976d84dc9fabfcc5d26e6
sohei.kakihara <souhei_kakihara@simple-inc.com>
2025-05-12 23:32:51 +0900

    スタイルが反映されない問題を修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Nav from './components/Nav';
 import SessionWrapper from './components/SessionWrapper';
+import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
