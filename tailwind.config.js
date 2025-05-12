1cc7f9513bb43d57887abe5d89927a0f499c1c0e
sohei.kakihara <souhei_kakihara@simple-inc.com>
2025-05-12 22:44:14 +0900

    fs依存を修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/tweets/page.tsx',
+    './app/register/page.tsx',
+    './app/login/page.tsx',
+    './app/components/**/*.tsx',
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
-}
+};
